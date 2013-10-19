c801cdb29be5e238e5fe8ddc52f499913f0e5299
Justin Barnard <justin@confine.com>
2013-10-18 20:24:01 -0500

    Working Scale test of Board Position and subsequent changes

--- src/scrabble/ScrabbleWord.java
+++ src/scrabble/ScrabbleWord.java
@@ -1,7 +1,10 @@
 package scrabble;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -10,34 +13,41 @@ import java.util.Set;
  */
 
 public class ScrabbleWord implements Comparable<ScrabbleWord> {
-	
+	final static Set<String> onePoint = new HashSet<String>(Arrays.asList("A","E","I","O","U","L","N","R","S","T"));
+	final static Set<String> twoPoint = new HashSet<String>(Arrays.asList("D","G"));
+	final static Set<String> threePoint = new HashSet<String>(Arrays.asList("B","C","M","P"));
+	final static Set<String> fourPoint = new HashSet<String>(Arrays.asList("F","H","V","W","Y"));
+	final static Set<String> fivePoint = new HashSet<String>(Arrays.asList("K"));
+	final static Set<String> eightPoint = new HashSet<String>(Arrays.asList("X","J"));
+	final static Set<String> tenPoint = new HashSet<String>(Arrays.asList("Q","Z"));
+	final static Set<String> zeroPoint = new HashSet<String>(Arrays.asList("a","b","c","d","e","f","g","h","i","j",
+			"k","l","m","n","o","p","q","r","s","t",
+			"u","v","w","x","y","z"));
+
 	private String word;
 	private int score;
-	private int boardPostion;
+	private int boardX;
+	private int boardY;
 
 	public ScrabbleWord() {
-		word = "Unintialized Word";
-		score = -99;
-	}
-	public ScrabbleWord(String _word, int _score) {
-		word = _word;
-		score = _score;
+		this.word = "default";
+		this.score = -1;
 
 	}
 	public ScrabbleWord(String _word) {
-		word = _word;
+		this.word =_word;
+		this.score = 0;
 	}
-	
-	
-	//Main Method for testing Execution
+	public ScrabbleWord(String _word, int _score){
+		this.word = _word;
+		this.score = _score;
+	}
+
+
+	/*Main Method for testing Execution
 	public static void main(String[] args) {
-		
-		ScrabbleWord test = new ScrabbleWord("TeST");
-		computeScore(test);
-		System.out.printf("The Word is: %s \nThe Score is %d\n",test.toString(),test.getScore());
-		
-		
 	}
+	 */
 
 	public int getScore() {
 		return this.score;
@@ -45,59 +55,120 @@ public class ScrabbleWord implements Comparable<ScrabbleWord> {
 	public void setScore(int _newScore) {
 		score = _newScore;
 	}
+	public void setBoardPostion(int _x, int _y) {
+		this.boardX = _x;
+		this.boardY = _y;
+
+	}
+	public int getBoardXPos(){
+		return this.boardX;
+	}
+	public int getBoardYPos(){
+		return this.boardY;
+	}
+
 	@Override
 	public String toString() {
 		return this.word;		
 	}
-	@Override	
-	public int compareTo(ScrabbleWord _word){
-		return(this.getScore() - _word.getScore());
+
+	public boolean equals(ScrabbleWord _comparedWord){
+		return this.getScore() == _comparedWord.getScore();
+
+	}
+	@Override //Use Descending Natural Ordering	
+	public int compareTo(ScrabbleWord _comparedWord){
+		if(this.equals(_comparedWord)) {
+			return 0;
+		}
+		else if(this.getScore() > _comparedWord.getScore()){
+			return -1;
+		}
+		else
+			return 1;
 	}
 
-	public static ScrabbleWord computeScore(ScrabbleWord _word) {
-		final Set<String> onePoint = new HashSet<String>(Arrays.asList("A","E","I","O","U","L","N","R","S","T"));
-		final Set<String> twoPoint = new HashSet<String>(Arrays.asList("D","G"));
-		final Set<String> threePoint = new HashSet<String>(Arrays.asList("B","C","M","P"));
-		final Set<String> fourPoint = new HashSet<String>(Arrays.asList("F","H","V","W","Y"));
-		final Set<String> fivePoint = new HashSet<String>(Arrays.asList("K"));
-		final Set<String> eightPoint = new HashSet<String>(Arrays.asList("X","J"));
-		final Set<String> tenPoint = new HashSet<String>(Arrays.asList("Q","Z"));
-		final Set<String> zeroPoint = new HashSet<String>(Arrays.asList("a","b","c","d","e","f","g","h","i","j",
-																 "k","l","m","n","o","p","q","r","s","t",
-																 "u","v","w","x","y","z"));
+	public void computeScore() {
+
+
+		for(int c=0; c < this.toString().length(); c++) {
+			letterScore(this, c);
+		}
+	}
+	public void bestBoardPos(){
+		if(this.word.length() == 7) {
+			this.score += 50;
+			letterScore(this, 2);
+			this.setBoardPostion(1, 7);
+		}
+		else if(this.word.length() == 6) {
+			List<ScrabbleWord> tempWords = new ArrayList<ScrabbleWord>();
+			tempWords.add(new ScrabbleWord(this.word, this.score));
+			tempWords.add(new ScrabbleWord(this.word, this.score));
+			tempWords.add(new ScrabbleWord(this.word, this.score));
+			tempWords.add(new ScrabbleWord(this.word, this.score));
+			
+			letterScore(tempWords.get(0), 0);
+			letterScore(tempWords.get(1), 5);
+			letterScore(tempWords.get(2), 1);
+			letterScore(tempWords.get(3), 4);
+			tempWords.get(0).setBoardPostion(3, 7);
+			tempWords.get(1).setBoardPostion(6, 7);
+			tempWords.get(1).setBoardPostion(2, 7);
+			tempWords.get(1).setBoardPostion(7, 7);
+			Collections.sort(tempWords);
+			this.score = tempWords.get(0).getScore();
+			this.boardX = tempWords.get(0).boardX;
+			this.boardY = tempWords.get(0).boardY;
+		}
+		else if(this.word.length() == 5) {
+			List<ScrabbleWord> tempWords = new ArrayList<ScrabbleWord>();
+			tempWords.add(new ScrabbleWord(this.word, this.score));
+			tempWords.add(new ScrabbleWord(this.word, this.score));
+			letterScore(tempWords.get(0), 0);
+			letterScore(tempWords.get(1), 4);
+			tempWords.get(0).setBoardPostion(3, 7);
+			tempWords.get(1).setBoardPostion(7, 7);
+			Collections.sort(tempWords);
+			this.score = tempWords.get(0).getScore();
+			this.boardX = tempWords.get(0).boardX;
+			this.boardY = tempWords.get(0).boardY;
+		}
+		else if(this.word.length() == 4) {
+			this.setBoardPostion(4, 7);
+		}
+		else
+			System.out.println("Pass");
 		
-		for(int c=0; c < _word.toString().length(); c++) {
-			
-			if(onePoint.contains(Character.toString(_word.toString().charAt(c)))) {
-				_word.setScore(_word.getScore()+1);
-			}
-			else if(twoPoint.contains(Character.toString(_word.toString().charAt(c)))) {
-				_word.setScore(_word.getScore()+2);
-			}
-			else if(threePoint.contains(Character.toString(_word.toString().charAt(c)))) {
-				_word.setScore(_word.getScore()+3);
-			}
-			else if(fourPoint.contains(Character.toString(_word.toString().charAt(c)))) {
-				_word.setScore(_word.getScore()+4);
-			}
-			else if(fivePoint.contains(Character.toString(_word.toString().charAt(c)))) {
-				_word.setScore(_word.getScore()+5);
-			}
-			else if(eightPoint.contains(Character.toString(_word.toString().charAt(c)))) {
-				_word.setScore(_word.getScore()+8);	
-			}
-			else if(tenPoint.contains(Character.toString(_word.toString().charAt(c)))) {
-				_word.setScore(_word.getScore()+10);
-			}
-			else if(zeroPoint.contains(Character.toString(_word.toString().charAt(c)))) {
-				_word.setScore(_word.getScore());
-			}
-			else {
-				System.out.println("Invaild Word");
-				_word.setScore(0);
-				break;
-			}
-			
+	}
+	public static ScrabbleWord letterScore(ScrabbleWord _word, int c) {
+		if(onePoint.contains(Character.toString(_word.toString().charAt(c)))) {
+			_word.setScore(_word.getScore()+1);
+		}
+		else if(twoPoint.contains(Character.toString(_word.toString().charAt(c)))) {
+			_word.setScore(_word.getScore()+2);
+		}
+		else if(threePoint.contains(Character.toString(_word.toString().charAt(c)))) {
+			_word.setScore(_word.getScore()+3);
+		}
+		else if(fourPoint.contains(Character.toString(_word.toString().charAt(c)))) {
+			_word.setScore(_word.getScore()+4);
+		}
+		else if(fivePoint.contains(Character.toString(_word.toString().charAt(c)))) {
+			_word.setScore(_word.getScore()+5);
+		}
+		else if(eightPoint.contains(Character.toString(_word.toString().charAt(c)))) {
+			_word.setScore(_word.getScore()+8);	
+		}
+		else if(tenPoint.contains(Character.toString(_word.toString().charAt(c)))) {
+			_word.setScore(_word.getScore()+10);
+		}
+		else if(zeroPoint.contains(Character.toString(_word.toString().charAt(c)))) {
+			_word.setScore(_word.getScore());
+		}
+		else {
+			System.out.println("Invaild Word");
+			_word.setScore(0);
 		}
 		return _word;
 	}
--- src/scrabble/wordScore.java
+++ src/scrabble/wordScore.java
@@ -1,62 +1,34 @@
-/**
- * Deprecated, Code Folded into ScrabbleWord
- */
-
 package scrabble;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import scrabble.ScrabbleWord;
+
 
 public class wordScore {
+	
 
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		final Set<String> onePoint = new HashSet<String>(Arrays.asList("A","E","I","O","U","L","N","R","S","T"));
-		final Set<String> twoPoint = new HashSet<String>(Arrays.asList("D","G"));
-		final Set<String> threePoint = new HashSet<String>(Arrays.asList("B","C","M","P"));
-		final Set<String> fourPoint = new HashSet<String>(Arrays.asList("F","H","V","W","Y"));
-		final Set<String> fivePoint = new HashSet<String>(Arrays.asList("K"));
-		final Set<String> eightPoint = new HashSet<String>(Arrays.asList("X","J"));
-		final Set<String> tenPoint = new HashSet<String>(Arrays.asList("Q","Z"));
-		final Set<String> zeroPoint = new HashSet<String>(Arrays.asList("a","b","c","d","e","f","g","h","i","j",
-																 "k","l","m","n","o","p","q","r","s","t",
-																 "u","v","w","x","y","z"));
-		String word ="ABCDEF?";
-		int wordScore = 0;
-		for(int c=0; c < word.length(); c++) {
-		
-			if(onePoint.contains(Character.toString(word.charAt(c)))) {
-				wordScore +=1;
-			}
-			else if(twoPoint.contains(Character.toString(word.charAt(c)))) {
-				wordScore +=2;
-			}
-			else if(threePoint.contains(Character.toString(word.charAt(c)))) {
-				wordScore +=3;
-			}
-			else if(fourPoint.contains(Character.toString(word.charAt(c)))) {
-				wordScore +=4;
-			}
-			else if(fivePoint.contains(Character.toString(word.charAt(c)))) {
-				wordScore +=5;
-			}
-			else if(eightPoint.contains(Character.toString(word.charAt(c)))) {
-				wordScore +=8;	
-			}
-			else if(tenPoint.contains(Character.toString(word.charAt(c)))) {
-				wordScore +=10;
-			}
-			else if(zeroPoint.contains(Character.toString(word.charAt(c)))) {
-				wordScore +=0;
-			}
-			else {
-				System.out.println("Invaild Word");
-				break;
-			}
-			
+		ScrabbleWord FinalWord;
+		List<String> list = Arrays.asList("TEST","PIMP","FACE","ZEBRA","QuDoBa");
+		List<ScrabbleWord> scoredWords = new ArrayList<ScrabbleWord>();
+		int i = 0;
+		for(String s :list) {	
+			scoredWords.add(new ScrabbleWord(s));
+			scoredWords.get(i).computeScore();
+			i++;
 		}
-		System.out.printf("%s is worth %d points",word,wordScore);
-		
+		System.out.println( scoredWords.toString());
+		Collections.sort(scoredWords);
+		scoredWords = scoredWords.subList(0, 3);
+		System.out.println( scoredWords.toString());
+		for(i= 0; i <=2; i++){
+			scoredWords.get(i).bestBoardPos();
+		}
+		Collections.sort(scoredWords);
+		System.out.println( scoredWords.toString());
+		FinalWord = scoredWords.get(0);
+		System.out.printf("%s was placed at row %d postion %d for a score of %d",FinalWord,FinalWord.getBoardYPos(),FinalWord.getBoardXPos(),FinalWord.getScore());
 		
 	}
 
